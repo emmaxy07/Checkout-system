4ed098adc9f81f47a5275cf5c8da179fd838d002
Samuel Adebayo <kurdzsamuel@gmail.com>
2021-08-07 16:44:48 +0100

    fetch products in app component

--- src/App.js
+++ src/App.js
@@ -1,80 +1,26 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from 'axios';;
 
-function App(){
-  const PAGE_PRODUCTS = 'products';
-  const PAGE_CART = 'cart';
-  const [cart, setCart] = useState([]);
-  const [page, setPage] = useState('PAGE_PRODUCTS'); 
+const App = () => {
+  const [products, SetProducts] =useState([]);
 
-  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-  const apiURL = "https://fakestoreapi.com/products";
-
-  const fetchData = async () => {
-    const response = await axios.get(apiURL)
-
-    this.setProducts(response.data)
+  function fetchProducts() {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(productsData => SetProducts(productsData));
   }
-
-  const addToCart = (product) =>{
-    setCart([...cart, product]);
-    fetchData();
-  };
-
-  const navigateTo = (nextPage) => {
-    setPage(nextPage);
-  }
-
-  const renderProducts = () =>(
-      <>
-      <h1>Products</h1>
-    <div className="products">
-      {products.map((product, idx) =>(
-      <div className="product" key={idx}>
-        <h3>{product.id}</h3>
-        <h3>{product.title}</h3>
-        <h4>{product.price}</h4>
-      <h4>{product.description}</h4>
-      <h4>{product.category}</h4>
-      <img src={product.image} alt={product.title} />
-      <button onClick={() => addToCart(product)}>Add to cart</button>
-    </div>
-    ))}
-    </div>
-    </>
-  );
-  
-  const renderCart = () => (
-    <>
-      <h1>Cart</h1>
-    <div className="products">
-      {cart.map((product, idx) =>(
-      <div className="product" key={idx}>
-        <h3>{product.id}</h3>
-        <h3>{product.title}</h3>
-        <h4>{product.price}</h4>
-      <h4>{product.description}</h4>
-      <h4>{product.category}</h4>
-      <img src={product.image} alt={product.title} />
-      {/*<button onClick={() =>addToCart(product)>Add to cart</button>*/}
-    </div>
-    ))}
-    </div>
-    </>
-  );
 
   return (
     <div className="App">
-    <header>
-    <button onClick={() => navigateTo(PAGE_CART)}>Go to cart({cart.length})</button>
-    <button onClick={() => navigateTo(PAGE_PRODUCTS)}>View Products</button>
-    </header>
-    {page === PAGE_PRODUCTS && renderProducts()}
-    {page === PAGE_CART && renderCart()}
+      <p>Apple</p>
+      <button onClick={fetchProducts}>Click to get products</button>
+      <p>{products.length}</p>
     </div>
-  );
-}
+  )
+};
 
 export default App;
